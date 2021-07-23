6920564c0bb49f44b58ece859f3a93c80d66229f
Wenjie Huang <mf1933036@smali.nju.edu.cn>
2021-07-23 20:15:12 +0800

    the starter(entrance) of Coral

---
+++ coral-example/src/main/java/zhangyi/adapter/coral/CoralStarter.java
@@ -0,0 +1,12 @@
+package zhangyi.adapter.coral;
+
+import sqlline.SqlLine;
+
+import java.io.IOException;
+
+public class CoralStarter {
+
+    public static void main(String[] args) throws IOException {
+        SqlLine.main(args);
+    }
+}
